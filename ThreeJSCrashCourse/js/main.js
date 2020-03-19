1b15355304dc5ac57f05287ac0e166f12824f9aa
TheStevenD <51217148+TheStevenD@users.noreply.github.com>
2020-03-19 07:26:41 -0700

    added orbit controls, proper scaling and now the css works too.

--- ThreeJSCrashCourse/js/main.js
+++ ThreeJSCrashCourse/js/main.js
@@ -1,9 +1,18 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+window.addEventListener( 'resize', function()
+{
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+});
 
 // create the shape
 var geometry = new THREE.BoxGeometry(1, 1, 1);
