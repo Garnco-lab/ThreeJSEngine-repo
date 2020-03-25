fe76f47c3e6b3b8dd6afa6f9504184b403cb5934
TheStevenD <51217148+TheStevenD@users.noreply.github.com>
2020-03-25 11:23:12 -0700

    added a texture and updated it

--- ThreeJSCrashCourse/js/main.js
+++ ThreeJSCrashCourse/js/main.js
@@ -18,9 +18,18 @@ var controls = new THREE.OrbitControls(camera,renderer.domElement);
 
 // create the shape
 var geometry = new THREE.BoxGeometry(1, 1, 1);
+var cubeMaterials = 
+[
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/cube1.png'), side: THREE.DoubleSide}), // Right side
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/cube1.png'), side: THREE.DoubleSide}), // Left Side
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/cube1.png'), side: THREE.DoubleSide}), // Top Side
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/cube1.png'), side: THREE.DoubleSide}), // Bottom Side
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/cube1.png'), side: THREE.DoubleSide}), // Front side
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/cube1.png'), side: THREE.DoubleSide}), // Back side
+]
 
 // create a material, color or image texture
-var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: true});
+var material = new THREE.MeshFaceMaterial(cubeMaterials);
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
